f2460320b6d913f611ae181813018664f8a0a0bd
aguirrefab <aguirrefabian.unne@gmail.com>
2024-02-14 01:04:02 -0300

    update image logo and header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,17 @@
-import './Header.css'
+import devLogo from '../../assets/images/duck.jpg';
+import './Header.css';
+
 export const Header = () => {
   return (
-    <div>
-      <h1>!Blockbuster</h1>
-    </div>
-  )
-}
+    <>
+      <div>
+        <a href='https://github.com/aguirrefab' target='_blank'>
+          <img src={devLogo} className='brand-img' alt='@aguirrefab' />
+        </a>
+      </div>
+      <div>
+        <h1>!Blockbuster</h1>
+      </div>
+    </>
+  );
+};
